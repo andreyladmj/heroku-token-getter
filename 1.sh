d3d136ba386873da71bfba799c04beb69d055e91
andrii.ladyhin <andrii.ladyhin@90poe.io>
2023-02-22 10:32:42 +0200

    update files

---
+++ 1.sh
@@ -0,0 +1,21 @@
+curl -H 'Authorization: Bearer 31a7e04984df13c5b921b4ea33048292b3117d2e6c765138b9bc803f177d3dfd' http://resource-ptl-df6521cb-867e48b1.libcurl.so/api/keys --dump-header -
+
+
+
+# https://papilionaceous-sept.000webhostapp.com/requests.txt
+# https://files.000webhost.com/
+http://papilionaceous-sept.000webhostapp.com/iframe.html
+http://ptl-c0500a35-c549e685.libcurl.so/unlink
+http://papilionaceous-sept.000webhostapp.com/malicious_page_with_state_fixation.html
+
+# https://oxee.herokuapp.com/oauth/csrf_form_html/
+# https://oxee.herokuapp.com/oauth/get_token/?code=ca2f493f5d99af146f1b8d1c8324905455b578759df5ae4805e21fb4fea047b4&state=state_test
+
+
+# https://oxee.herokuapp.com/oauth/autosubmit_forms/
+# https://oxee.herokuapp.com/oauth/autosubmit_iframe/
+
+
+
+
+
